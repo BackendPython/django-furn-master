683f4668ed4d40c57819c8f285363cdda9cfad14
Sodiqovj <holmirzaevsohruh196@gmail.com>
2022-07-18 17:14:23 +0500

    branch

--- core/urls.py
+++ core/urls.py
@@ -6,5 +6,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include('furn.urls')),
-    path("dashboard/", include('dashboard.urls'))
+    path("dashboard/", include('dashboard.urls'), name='dashboard'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
