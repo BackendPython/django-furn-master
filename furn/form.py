f25c1316339d54eb6fc2748b3a0d1b10b3175b5f
backendpython <holmirzaevsohruh196@gmail.com>
2022-08-03 13:53:25 +0500

    main

--- furn/form.py
+++ furn/form.py
@@ -5,7 +5,7 @@ from django import forms
 
 User = get_user_model()
 
-class Registertration(UserCreationForm):
+class RegistertrationDetails(UserCreationForm):
     first_name = forms.CharField(
         max_length=100,
         required = True,
@@ -29,6 +29,32 @@ class Registertration(UserCreationForm):
             "class" : "form-control",
             "placeholder" : "Emailni kiriting"
         })
+    )
+    password1 = forms.CharField(
+        max_length=100,
+        required = True,
+        widget = forms.TextInput(attrs={
+            "class" : "form-control",
+            "placeholder" : "Parolingizni kiriting"
+        })
+    )
+    password2 = forms.CharField(
+        max_length=100,
+        required = True,
+        widget = forms.TextInput(attrs={
+            "class" : "form-control",
+            "placeholder" : "Parolingizni qayta kiriting"
+        })
+    )
+
+class Regsitration(UserCreationForm):
+    model = RegistertrationDetails
+    fields = (
+        "first_name",
+        "last_name",
+        "email",
+        "password1",
+        "password2"
     )
 
 
