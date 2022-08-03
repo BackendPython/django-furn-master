3adcf507292f596df06a0f9617baa5c46897ced8
backendpython <holmirzaevsohruh196@gmail.com>
2022-08-03 13:43:07 +0500

    main

--- furn/form.py
+++ furn/form.py
@@ -1,13 +1,34 @@
-from dataclasses import fields
-from django import forms
 from django.contrib.auth.forms import UserCreationForm, UsernameField
 from django.contrib.auth import get_user_model
+from dataclasses import fields
+from django import forms
 
 User = get_user_model()
 
 class Registertration(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ("username", )
-        field_classes = {"username": UsernameField}
+    first_name = forms.CharField(
+        max_length=100,
+        required = True,
+        widget = forms.TextInput(attrs={
+            "class" : "form-control",
+            "placeholder" : "Familiyangizni kiriting"
+        })
+    )
+    last_name = forms.CharField(
+        max_length=100,
+        required = True,
+        widget = forms.TextInput(attrs={
+            "class" : "form-control",
+            "placeholder" : "Familiyangizni kiriting"
+        })
+    )
+    email = forms.EmailField(
+        max_length=100,
+        required = True,
+        widget = forms.TextInput(attrs={
+            "class" : "form-control",
+            "placeholder" : "Familiyangizni kiriting"
+        })
+    )
+
 
