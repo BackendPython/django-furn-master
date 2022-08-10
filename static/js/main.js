4a6a0c969e9806a7165100f6e8ad21b4339d0c1c
backendpython <holmirzaevsohruh196@gmail.com>
2022-08-10 15:22:48 +0500

    main

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,14 @@
 let logout_button = document.querySelector('.logout-navbar')
 let block_content = document.querySelector('.block-content')
 let logout_coniform = document.querySelector('.logout-coniform')
+console.log(logout_coniform);
 
 logout_button.addEventListener('click', function(){
+    logout_coniform.ariaPressed = true
+    logout_coniform.ariaSelected = true
     logout_coniform.style.display = 'block'
-    block_content.style.mixBlendMode = 'darken'
+    block_content.style.opacity = '0.3'
 })
+setInterval(() => {
+    console.log(logout_coniform);
+}, 2000);
