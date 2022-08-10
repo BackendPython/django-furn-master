0a26a4ffe8ed069ac886aa6323707aa0f8d2be9b
backendpython <holmirzaevsohruh196@gmail.com>
2022-08-10 14:59:36 +0500

    main

---
+++ static/js/main.js
@@ -0,0 +1,7 @@
+let logout_button = document.querySelector('.logout-navbar')
+let block_content = document.querySelector('.block-content')
+let logout_coniform = document.querySelector('.logout-coniform')
+
+logout_button.addEventListener('click', function(){
+    logout_coniform.style.display = 'block'
+})
