8a0e89072982185a44ff4bd646bfaa8341daeffd
backendpython <holmirzaevsohruh196@gmail.com>
2022-08-15 14:15:16 +0500

    main

--- furn/models.py
+++ furn/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from  django.contrib.auth.models import AbstractUser
 
 class MyUser(AbstractUser):
-    username = None
     email = models.EmailField(unique=True)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
