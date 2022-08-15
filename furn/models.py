51e441f8d971e996ad11decd3ca5fdb7dd8b52eb
backendpython <holmirzaevsohruh196@gmail.com>
2022-08-15 15:10:12 +0500

    main

--- furn/models.py
+++ furn/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from  django.contrib.auth.models import AbstractUser
 
 class MyUser(AbstractUser):
+    username = models.CharField(auto_created=True, max_length=50)
     email = models.EmailField(unique=True)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
