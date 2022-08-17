4e616003401876347e03121659f00890bd882441
backendpython <holmirzaevsohruh196@gmail.com>
2022-08-17 14:17:58 +0500

    main

--- furn/signal.py
+++ furn/signal.py
@@ -1,14 +1,14 @@
 from django.db.models.signals import post_save
 from django.contrib.auth import get_user_model
 from django.dispatch import receiver
-from .models import MyUser, Profile
+from .models import Profile
 
 User =  get_user_model()
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instace, created, **kwargs):
     if created:
-        Profile.objects.create(user=instace)
+        Profile.objects.create(custome_user=instance)
         
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
