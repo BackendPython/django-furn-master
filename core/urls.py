df00b8e0f5826bdb578c382708ae77330fb17ea3
backendpython <holmirzaevsohruh196@gmail.com>
2022-09-30 14:32:05 +0500

    main

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,21 @@
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
+from django.contrib import admin
 from django.conf.urls.static import static
+from django.urls import path, include, re_path
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include('furn.urls')),
     path("dashboard/", include('dashboard.urls'), name='dashboard'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
++ i18n_patterns(
+    path('i18n/', include('django.conf.urls.i18n')),
+    path('', include('translate.urls')),
+    prefix_default_language=False
+)
+
+if 'rosetta' in settings.INSTALLED_APPS:
+    urlpatterns += [
+        re_path(r'^rosetta/', include('rosetta.urls'))
+    ]
