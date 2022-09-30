2010ee5ba98ffef551e270ede5dd1b0bdddba449
backendpython <holmirzaevsohruh196@gmail.com>
2022-09-30 14:56:14 +0500

    main

--- furn/views.py
+++ furn/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.db.models import Q
 from django.db.models import Avg
