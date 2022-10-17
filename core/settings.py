6739b68ba6676af1397234677cd6fb71189b4a1f
backendpython <holmirzaevsohruh196@gmail.com>
2022-10-17 14:09:21 +0500

    install djangorestframework 2

--- core/settings.py
+++ core/settings.py
@@ -158,3 +158,11 @@ LOCALE_PATHS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'furn.MyUser'
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
