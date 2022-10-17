5144d62fab6d1738c7e7e7036e43498126cf1e69
backendpython <holmirzaevsohruh196@gmail.com>
2022-10-17 15:18:16 +0500

    carousel id

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,6 @@ from api.views import *
 app_name = 'api'
 
 urlpatterns = [
-    path('carousel', carousel_api, name='carousel')
+    path('carousel', carousel, name='carousel'),
+    path('carousel-add', carousel_add, name='carousel_add'),
 ]
--- api/views.py
+++ api/views.py
@@ -7,9 +7,26 @@ from rest_framework.decorators import api_view, permission_classes
 # main API
 @api_view(['GET'])
 @permission_classes((permissions.AllowAny,))
-def carousel_api(request):
+def carousel(request):
     carousel = Carousel.objects.all()
     serializer = CarouselApi(carousel, many=True)
+    return Response(serializer.data)
+
+# add carousel
+@api_view(['POST'])
+@permission_classes((permissions.AllowAny,))
+def carousel_add(request):
+    serializer = CarouselApi(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+# search carousel with id
+@api_view(["GET"])
+@permission_classes((permissions.AllowAny, ))
+def singleapi(request, pk):
+    carousel = Carousel.objects.get(id=pk)
+    serializer = CarouselApi(carousel, many=False)
     return Response(serializer.data)
 
 
